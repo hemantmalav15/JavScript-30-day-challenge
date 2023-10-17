61f37cc10bab59b710270237402f03526a026c28
Hemant Malav <96330614+hemantmalav15@users.noreply.github.com>
2023-10-17 21:20:31 +0530

    Operator and conditional expression

---
+++ DAY-3/index.js
@@ -0,0 +1,46 @@
+/**
+ *  Operators in JS
+ * 
+ * 1. Arithmetic Operators
+ *    - Addition (a + b)
+ *    - Subtraction (a - b)
+ *    - Multiplicatio (a * b)
+ *    - Division (a / b)
+ *    - Modulo (a % b)
+ *    - Exponentiation (a^b -> a**b)
+ *    - Increment (a++)
+ *    - Decrement (a--)
+ * 
+ * 2. Assignment Operator
+ *    =, +=, -=, *=, /= etc.
+ * 
+ * 3. Comparison Operators
+ *    - Equal to ==
+ *    - Not Equal !=
+ *    - Equal value and type ===
+ *    - Both value and type not equal !==
+ *    - Greater than >
+ *    - Less than <
+ *    - Greater than equal >=
+ *    - Less than equal <=
+ *    - Ternary ?
+ * 
+ * 4. Logical Operators
+ *    - logical AND -> &&
+ *    - logical OR -> ||
+ *    - logical NOT -> !
+ */
+let a = 2;
+let b = "2";
+console.log("a == b: " , a == b) // returns true bcoz it only checks value and values are same
+console.log("a != b: " , a != b) // returns false bcoz it only checks value and values are same
+console.log("a !== b: " , a !== b) // returns true bcoz it checks value as well as type and type is different
+console.log("a === b: " , a === b); // false
+
+console.log("a power b: ", 2 ** 3); // it is 2^3 = 8
+
+/**
+ * Conditional Expression in JavaScript
+ *  - if else 
+ *  - if else if else
+ */
